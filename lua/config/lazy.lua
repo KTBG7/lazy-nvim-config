ee5ae4867366952f2b43ba1f84229d6136e9f4e2
Kevin Ibanez <kevin.ibanez.dev@gmail.com>
2025-01-05 12:15:14 -0600

    Re-enabling telescope after update to LazyVim 14w

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.editor.telescope" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
